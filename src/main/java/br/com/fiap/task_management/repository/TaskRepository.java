c835a7aca2fdb2199d138e1febc81ba6cf4a6350
diogo_gallina <diogogallina20@gmail.com>
2024-08-24 12:10:49 -0300

    feat: create TaskRepository

--- src/main/java/br/com/fiap/task_management/repository/TaskRepository.java
+++ src/main/java/br/com/fiap/task_management/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package br.com.fiap.task_management.repository;
 
-public interface TaskRepository {
+import br.com.fiap.task_management.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<TaskModel, Long> {
 }
