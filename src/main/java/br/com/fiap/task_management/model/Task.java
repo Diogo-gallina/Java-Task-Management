d526c0eff48de47fc080e2ff44c74fabf4b4d896
diogo_gallina <diogogallina20@gmail.com>
2024-08-24 12:04:57 -0300

    feat: create Task model

--- src/main/java/br/com/fiap/task_management/model/Task.java
+++ src/main/java/br/com/fiap/task_management/model/Task.java
@@ -1,4 +1,35 @@
 package br.com.fiap.task_management.model;
 
+import br.com.fiap.task_management.model.enunms.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter @Setter @NoArgsConstructor
+
+@Entity
+@Table(name = "CP1_JV_2TDSS_TASK")
+@EntityListeners(AuditingEntityListener.class)
 public class Task {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "task_id")
+    private Integer id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 700)
+    private String description;
+
+    @Column(name = "status", nullable = false, length = 700)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/br/com/fiap/task_management/model/enunms/Status.java
+++ src/main/java/br/com/fiap/task_management/model/enunms/Status.java
@@ -1,4 +1,5 @@
 package br.com.fiap.task_management.model.enunms;
 
 public enum Status {
+    PENDING, IN_PROGRESS, COMPLETED
 }
