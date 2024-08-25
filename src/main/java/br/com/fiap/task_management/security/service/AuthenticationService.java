f6c533d75aecb6ac7c1b32db6dd9adc73acfa4ec
diogo_gallina <diogogallina20@gmail.com>
2024-08-25 01:42:24 -0300

    feat: create authentication service

--- src/main/java/br/com/fiap/task_management/security/service/AuthenticationService.java
+++ src/main/java/br/com/fiap/task_management/security/service/AuthenticationService.java
@@ -1,4 +1,18 @@
 package br.com.fiap.task_management.security.service;
 
-public class AuthenticationService {
+import br.com.fiap.task_management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
 }
