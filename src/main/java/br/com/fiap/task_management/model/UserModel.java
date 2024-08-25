5cd1b4d1ac08b7e074cad3cd5dd6ed22c92b4fcf
diogo_gallina <diogogallina20@gmail.com>
2024-08-25 01:56:06 -0300

    refactor: add user security in usermodel

--- src/main/java/br/com/fiap/task_management/model/UserModel.java
+++ src/main/java/br/com/fiap/task_management/model/UserModel.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -15,12 +19,12 @@ import java.util.List;
 @Entity
 @Table(name = "CP1_JV_2TDSS_USER")
 @EntityListeners(AuditingEntityListener.class)
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue
     @Column(name = "user_id")
-    private Long userId;
+    private Long id;
 
     @Column(name = "name", nullable = false, length = 100)
     private String name;
@@ -34,4 +38,44 @@ public class UserModel {
     @OneToMany(mappedBy = "user")
     private List<TaskModel> tasks;
 
+    public UserModel(String name, String email, String passwordEncoded) {
+        this.name = name;
+        this.email = email;
+        this.password = passwordEncoded;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
