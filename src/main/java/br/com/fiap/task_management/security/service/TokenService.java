b374f2191724d19c882c5f496ee77d9adf21838c
diogo_gallina <diogogallina20@gmail.com>
2024-08-25 01:42:03 -0300

    feat: create token service

--- src/main/java/br/com/fiap/task_management/security/service/TokenService.java
+++ src/main/java/br/com/fiap/task_management/security/service/TokenService.java
@@ -1,4 +1,44 @@
 package br.com.fiap.task_management.security.service;
 
+import br.com.fiap.task_management.model.UserModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Service
 public class TokenService {
+    @Value("${api.token.secret}")
+    private String tokenPassword;
+
+    public String gerarToken(UserModel user){
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(this.tokenPassword);
+            return JWT.create()
+                    .withIssuer("FIAP")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+                    .sign(algoritmo);
+        } catch (JWTCreationException e){
+            throw new RuntimeException("Erro ao gerar token jwt");
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.tokenPassword);
+            return JWT.require(algorithm)
+                    .withIssuer("FIAP").
+                    build().
+                    verify(token).
+                    getSubject();
+        } catch (JWTVerificationException e){
+            throw new RuntimeException("Não foi possível validar o TokenJWT");
+        }
+    }
 }
