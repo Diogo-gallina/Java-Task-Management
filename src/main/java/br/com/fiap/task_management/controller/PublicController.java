15b031f915a424acdab36e985cf8c5030787a866
diogo_gallina <diogogallina20@gmail.com>
2024-08-25 01:57:14 -0300

    feat: create user controller

--- src/main/java/br/com/fiap/task_management/controller/PublicController.java
+++ src/main/java/br/com/fiap/task_management/controller/PublicController.java
@@ -1,4 +1,16 @@
 package br.com.fiap.task_management.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("public")
 public class PublicController {
+
+    @GetMapping("/status")
+    public ResponseEntity<PossibleTaskStatusDTO> findTaskStatus() {
+        return ResponseEntity.ok().body(new PossibleTaskStatusDTO());
+    }
 }
--- src/main/java/br/com/fiap/task_management/controller/UserController.java
+++ src/main/java/br/com/fiap/task_management/controller/UserController.java
@@ -1,4 +1,37 @@
 package br.com.fiap.task_management.controller;
 
+import br.com.fiap.task_management.dto.SignUpUserDTO;
+import br.com.fiap.task_management.dto.UserDetailsDTO;
+import br.com.fiap.task_management.model.UserModel;
+import br.com.fiap.task_management.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/signup")
+    @Transactional
+    public ResponseEntity<UserDetailsDTO> signup(@RequestBody @Valid SignUpUserDTO dto, UriComponentsBuilder uriBuilder) {
+        var user = new UserModel(dto.name(), dto.email(), passwordEncoder.encode(dto.password()));
+        userRepository.save(user);
+        var url = uriBuilder.path("/user/signup/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(url).body(new UserDetailsDTO(user));
+    }
+
 }
