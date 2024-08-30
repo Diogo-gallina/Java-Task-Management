44b183958aeac29c710c9a656c25fef5f4d7cbf0
diogo_gallina <diogogallina20@gmail.com>
2024-08-29 21:46:38 -0300

    feat: create Repositories

--- src/main/java/br/com/fiap/task_management/repository/TaskRepository.java
+++ src/main/java/br/com/fiap/task_management/repository/TaskRepository.java
@@ -1,7 +1,14 @@
 package br.com.fiap.task_management.repository;
 
 import br.com.fiap.task_management.model.TaskModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+
+    @Query("from TaskModel t where t.user.id = :userId")
+    Page<TaskModel> findAllByUserId(Long userId, Pageable page);
+
 }
--- src/main/java/br/com/fiap/task_management/repository/UserRepository.java
+++ src/main/java/br/com/fiap/task_management/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package br.com.fiap.task_management.repository;
 
 import br.com.fiap.task_management.model.UserModel;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
+    @Query("from UserModel u where u.email = :email")
     UserModel findByEmail(String email);
 
 }
